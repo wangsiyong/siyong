94f06aa40512207aeae77d0552af28d79ce482f4
wangsiyong <wangsiyong@12sigma.ai>
2018-03-26 09:21:37 +0800

    使用dcm2niiConverterX，批量转nii，文件名重组

---
+++ dicoms2niis_by_exe.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+
+import os
+from subprocess import Popen
+
+
+def dicoms2niis(exe_path, source_path, out_path):
+    """
+    Convert dicoms to nii(use dcm2niiConverterX.exe)
+    :param exe_path: where the exe in
+    :param source_path: dicom dir
+    :param out_path: output dir for niis
+    """
+    last_dicom_dir = ''
+    for roots, _, files in os.walk(source_path):
+        for _ in files:
+            nii_prefix = roots.replace(source_path + '\\', '').split('\\')[0]
+            nii_postfix = roots.split('\\')[-1]
+            nii_name = nii_prefix + '_' + nii_postfix
+            if roots == last_dicom_dir:
+                continue
+            last_dicom_dir = roots
+            print(nii_name + '.nii')
+            cmd = [exe_path, roots, out_path, nii_name]
+            process = Popen(cmd, shell=True)
+            process.communicate()
+
+
+if __name__ == '__main__':
+    dicoms2niis(r'D:\GitHub\siyong\exes\dcm2niiConverterX.exe', r'G:\剂量', r'G:\out')
